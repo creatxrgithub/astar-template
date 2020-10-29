7a32c60ddb2683608e329a6d35f756112ac9c3bc
creatxrgithub <creatxr@gmail.com>
2020-10-28 23:04:16 -0400

    Add files via upload

---
+++ demos/later-todo.js
@@ -0,0 +1,33 @@
+class LaterTodo {
+	constructor (t, callback) {
+		this.durationStep = 1;
+		this.duration = t;
+		/*
+		if(args.length===1) {
+			if(typeof args[args.length-1] === 'function') {
+				this.callback = args.pop();
+			}
+		}
+		*/
+		if(typeof callback != 'function') {
+			throw `${callback} is not a function`;
+		}
+		this.callback = callback;
+	}
+
+	todo(...args) {
+		//*
+		setTimeout(() => {
+			this.callback(...args);
+		}, this.duration*this.durationStep++);
+		//*/
+		/*//如果不用 es6 的箭頭函數，需保存指鍼
+		let self = this;
+		setTimeout(function () {
+			self.callback(...args);
+		}, self.duration*self.durationStep++);
+		//*/
+	}
+}
+
+module.exports = LaterTodo;
